8bcfbbee05e4b8a7f06b81c697e2d847c50a12c2
FabioPin <fajos.pinheiro@hotmail.com>
2022-10-19 12:18:00 +0100

    Second-Commit

---
+++ src/components/Parent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        name: 'Frarthur'
+    };
+  };
+
+  render() {
+    return <div></div>;
+  }
+}
